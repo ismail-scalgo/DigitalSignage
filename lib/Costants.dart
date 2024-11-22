d170bfaff7517e80c27cbd57e2de7548ae79f6f7
ismail-scalgo <ismail.b@scalgo.net>
2024-11-22 12:07:22 +0530

    youtube video player changes

--- lib/Costants.dart
+++ lib/Costants.dart
@@ -2,11 +2,11 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:toastification/toastification.dart';
 
-String BASEURL = "https://web-dev-sgdsignage.scalgo.net";
+String BASEURL = "https://web-test-sgdsignage.scalgo.net";
 //String BASEURL = "http://192.168.0.98:8000";
 List<String> mediaTypes = ["jpeg", "mp4"];
 String PLATFORM = 'WEB';
-String HARDCODEPLATFORM = 'ANDROIDTV';
+String HARDCODEPLATFORM = 'ANDROID';
 double gheight = 0;
 double gwidth = 0;
 String SOCKET_ADDRESS = 'ws://web-dev-sgdsignage.scalgo.net/ws/';
