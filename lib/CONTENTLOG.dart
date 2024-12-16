05b5d3aa70a7263698f156e55e409bca2f74fa72
Jishnu Prabhakaran <jishnu.p@scalgo.net>
2024-12-16 17:45:20 +0530

    bug fix on broadcast start time and end time

--- lib/CONTENTLOG.dart
+++ lib/CONTENTLOG.dart
@@ -1,263 +1,3 @@
-// import 'dart:async';
-
-// import 'package:flutter/material.dart';
-// import 'package:hive/hive.dart';
-// import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// late Box buffer_box;
-// late Box recorded_box;
-// late SharedPreferences prefs;
-// String? screenCode = prefs.getString('NewScreenCode');
-// StreamController controller = StreamController();
-// Stream stream = controller.stream;
-
-// String CONTENT_NAME_KEY="content_name";
-// String CONTENT_DURATION_KEY="content_duration";
-// String START_TIME_KEY="start_time";
-
-// Future init() async
-// {
-
-//   prefs = await SharedPreferences.getInstance();
-
-//   bool connection_result = await InternetConnection().hasInternetAccess;
-//   buffer_box = await Hive.openBox("buffer_box");
-
-//   recorded_box = await Hive.openBox("recorded_box");
-
-//   stream.listen((value) async {
-
-//     Map eventValue=value;
-
-//     if(eventValue['event']=="content_add_event");
-//     {
-//       addlogToBuffer(broadcast_id:  eventValue['broadcast_id'],contentname:  eventValue['content_name'],contentDuration:  eventValue['content_duration'],zoneno:  eventValue['content_zone']);
-//     }
-//     if(eventValue['event']=="broadcast_end_event")
-//     {
-//       addtoRecordFromBufferOnEnd(broadcast_id:  eventValue['broadcast_id']);
-//     }
-
-//   });
-
-// }
-
-// Future addlogToBuffer({required String broadcast_id,required String contentname,required double contentDuration,required int zoneno}) async
-// {
-
-//       DateTime dateTime = DateTime.now();
-
-//     var buffer_datas =  buffer_box.keys;
-//    //IF ANY PENDING BROADCAST IN THE BUFFER
-//     if(buffer_datas.length > 1)
-//     {
-
-//       buffer_box.keys.forEach((key) async {
-
-//         if(key != broadcast_id)
-//         {
-
-//            await addtoRecordFromBufferOnEnd(broadcast_id:  key);
-
-//         }
-
-//       });
-
-//     }
-
-//       //if it is first no logs created
-//       //create new
-//       if(!buffer_box.containsKey(broadcast_id))
-//       {
-
-//         Map content_value={
-//           zoneno : {
-//               CONTENT_NAME_KEY:contentname,
-//               CONTENT_DURATION_KEY:contentDuration,
-//               START_TIME_KEY:dateTime
-
-//           } };
-
-//         buffer_box.put(broadcast_id, content_value);
-
-//       }
-//       else
-//       {
-
-//         // current zone entry is new
-
-//         Map bufferdata=buffer_box.get(broadcast_id);
-
-//         if(bufferdata.containsKey(zoneno))
-//         {
-
-//               addtoRecordOnNormalCondition(broadcast_id, bufferdata, zoneno);
-
-//             //********* ADD TO RECORDED DATA**********//
-
-//         }
-
-//         bufferdata[zoneno]  =  {
-//               CONTENT_NAME_KEY:contentname,
-//               CONTENT_DURATION_KEY:contentDuration,
-//               START_TIME_KEY:dateTime
-
-//         };
-
-//         buffer_box.put(broadcast_id, bufferdata);
-
-//       }
-
-// }
-
-// Future addtoRecordOnNormalCondition(String broadcast_id,Map bufferdata,int zone) async
-// {
-
-//     String content_name=bufferdata[broadcast_id][zone][CONTENT_NAME_KEY];
-//     Map? recorded_data=recorded_box.get(broadcast_id);
-
-//     //VERY FIRST TIME WITHOUT CONTAINS BROADCAST ID
-//     if(!recorded_box.containsKey(broadcast_id))
-//     {
-//       String content_name=bufferdata[broadcast_id][zone][CONTENT_NAME_KEY];
-//       double current_duration=bufferdata[broadcast_id][zone][CONTENT_DURATION_KEY];
-
-//        recorded_box.put(broadcast_id, {content_name:current_duration});
-
-//     }
-//     //ALREADY BROADCAST ADDED BUT THE CONTENT KEY IS FOR FIRST TIME
-//     else if(!recorded_data!.containsKey(content_name))
-//     {
-//       String content_name=bufferdata[broadcast_id][zone][CONTENT_NAME_KEY];
-//       double current_duration=bufferdata[broadcast_id][zone][CONTENT_DURATION_KEY];
-//       recorded_data[content_name]=current_duration;
-//       recorded_box.put(broadcast_id, recorded_data);
-//     }
-//     //ALREADY BROADCAST ADDED ALSO CONTENT ADDED
-//     else if(recorded_data.containsKey(content_name))
-//     {
-//         double past_duration = recorded_data[content_name];
-//         double current_duration = bufferdata[broadcast_id][zone][CONTENT_DURATION_KEY];
-//         double updated_duration = past_duration+current_duration;
-//         recorded_data[content_name] = current_duration;
-//         recorded_box.put(broadcast_id, recorded_data);
-//     }
-
-// }
-
-// Future addtoRecordFromBufferOnEnd({required String? broadcast_id}) async
-// {
-
-//      // check any logs on buffer and add to to recorded data
-
-//     Map? bufferdata=buffer_box.get(broadcast_id);
-//     DateTime dateTimeNow = DateTime.now();
-//     if(bufferdata!=null)
-//     {
-
-//       Map? recorded_data=recorded_box.get(broadcast_id);
-
-//       bufferdata.forEach((key, value) {
-//          String content_name=value[CONTENT_NAME_KEY];
-//          DateTime start_time=value[START_TIME_KEY];
-
-//            int current_duration=dateTimeNow.difference(start_time).inSeconds;
-
-//              if(!recorded_box.containsKey(broadcast_id))
-//               {
-
-//                  recorded_box.put(broadcast_id, {content_name:current_duration});
-
-//               }
-//               //ALREADY BROADCAST ADDED BUT THE CONTENT KEY IS FOR FIRST TIME
-//     else if(!recorded_data!.containsKey(content_name))
-//     {
-//       String content_name=value[CONTENT_NAME_KEY];
-//       int duration=dateTimeNow.difference(start_time).inSeconds;
-//       recorded_data[content_name]=duration;
-//       recorded_box.put(broadcast_id, recorded_data);
-//     }
-//      //ALREADY BROADCAST ADDED ALSO CONTENT ADDED
-//     else if(recorded_data.containsKey(content_name))
-//     {
-//         double past_duration = recorded_data[content_name];
-//         int current_duration = dateTimeNow.difference(start_time).inSeconds;
-//         double updated_duration = past_duration+current_duration;
-//         recorded_data[content_name] = updated_duration;
-//         recorded_box.put(broadcast_id, recorded_data);
-//     }
-
-//       },);
-
-//       // ADD TO SERVER //
-
-//       addToServer();
-
-//     buffer_box.delete(broadcast_id);
-
-//     }
-//     else
-//     {
-
-//     }
-
-// }
-
-// void addToServer()
-// {
-//     print("******* ADD TO SERVER CALLEDDDDDDDDDDD  ***********");
-//     print(recorded_box.keys);
-//     print(recorded_box.values);
-
-// }
-
-// void printValues()
-// {
-
-//   print("BUFFER VALUES");
-//   print(buffer_box.keys);
-//   print(buffer_box.values);
-
-//   print("RECORDED VALUES");
-//   print(recorded_box.keys);
-//   print(recorded_box.values);
-
-// }
-
-// // bufferdata={
-
-// //   broadcast_id:    {
-
-// //     zoneno : {
-// //               CONTENT_NAME_KEY:contentname,
-// //               CONTENT_DURATION_KEY:contentDuration,
-// //               START_TIME_KEY:dateTime
-
-// //               }
-// //                   };
-
-// //     zoneno : {
-// //
-// //               CONTENT_NAME_KEY:contentname,
-// //               CONTENT_DURATION_KEY:contentDuration,
-// //               START_TIME_KEY:dateTime
-
-// //               }
-// //                   };
-
-// //   }
-// // }
-
-//             // recorded_data={
-//             //   broadcast_id : {
-//             //     CONTENT_NAME_KEY: contentDuration
-
-//             //   }
-//             // }
-
-// ignore_for_file: non_constant_identifier_names, unused_local_variable
-
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
@@ -271,6 +11,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 late Box buffer_box;
 late Box recorded_box;
+late Box last_saved_time_box;
 late SharedPreferences prefs;
 String? screenCode = prefs.getString('NewScreenCode');
 StreamController controller = StreamController();
@@ -279,16 +20,22 @@ String CONTENT_NAME_KEY = "content_name";
 String CONTENT_DURATION_KEY = "content_duration";
 String START_TIME_KEY = "start_time";
 String IS_COMPLETED_KEY = "is_completed";
+String BROADCAST_START_TIME_KEY ="broadcast_start_datetime";
+String BROADCAST_END_TIME_KEY="broadcast_end_datetime";
+DateTime LAST_SAVED_TIME =DateTime.now();
+String LAST_SAVED_TIME_KEY ="last_saved_time_key";
+bool IS_FIRST=true;
 
 Future init() async {
-  Directory appDocumentDir =
-      await getApplicationDocumentsDirectory(); // Get the app's document directory
+  Directory appDocumentDir = await getApplicationDocumentsDirectory(); // Get the app's document directory
   Hive.init(appDocumentDir.path);
   prefs = await SharedPreferences.getInstance();
 
   buffer_box = await Hive.openBox("buffer_box");
 
   recorded_box = await Hive.openBox("recorded_box");
+
+  last_saved_time_box=await Hive.openBox("last_saved_time");
 
   stream.listen((value) async {
 
@@ -306,6 +53,28 @@ Future init() async {
       addtoRecordFromBufferOnEnd();
     }
   });
+
+
+
+}
+
+
+void startPeriodicTime()
+{
+
+  if(IS_FIRST)
+  {
+      Timer.periodic(Duration(seconds: 3), (time){
+
+            LAST_SAVED_TIME=DateTime.now();
+
+            last_saved_time_box.put(LAST_SAVED_TIME_KEY , LAST_SAVED_TIME);
+
+  });
+  }
+
+  IS_FIRST=false;                            
+
 }
 
 Future addlogToBuffer(
@@ -322,14 +91,19 @@ Future addlogToBuffer(
     buffer_box.keys.forEach((key) async {
       if (key != broadcast_id) {
         await addtoRecordFromBufferOnEnd();
+
       }
     });
   }
 
+  startPeriodicTime();
   //if it is first no logs created
   //create new
   if (!buffer_box.containsKey(broadcast_id)) {
+
+    
     Map content_value = {
+    BROADCAST_START_TIME_KEY : dateTime,
       zoneno: {
         CONTENT_NAME_KEY: contentname,
         CONTENT_DURATION_KEY: contentDuration,
@@ -337,7 +111,7 @@ Future addlogToBuffer(
       }
     };
 
-    buffer_box.put(broadcast_id, content_value);
+   await buffer_box.put(broadcast_id, content_value);
   } else {
     print("broadcast id already added");
     // current zone entry is new
@@ -348,7 +122,7 @@ Future addlogToBuffer(
       print("zone already addded");
       await addtoRecordOnNormalCondition(broadcast_id, bufferdata, zoneno);
 
-      //********* ADD TO RECORDED DATA**********//
+      // ********* ADD TO RECORDED DATA ********** //
     }
 
     bufferdata[zoneno] = {
@@ -357,7 +131,7 @@ Future addlogToBuffer(
       START_TIME_KEY: dateTime
     };
 
-    buffer_box.put(broadcast_id, bufferdata);
+   await buffer_box.put(broadcast_id, bufferdata);
   }
   printValues();
 }
@@ -376,9 +150,10 @@ Future addtoRecordOnNormalCondition(
         (buffer_box.get(broadcast_id))[zone][CONTENT_NAME_KEY];
     double current_duration =
         (buffer_box.get(broadcast_id))[zone][CONTENT_DURATION_KEY];
+    String broadcast_start_time=buffer_box.get(broadcast_id)[BROADCAST_START_TIME_KEY].toString();
 
-    recorded_box
-        .put(broadcast_id, {content_name: current_duration, IS_COMPLETED_KEY: false});
+   await recorded_box
+        .put(broadcast_id, {content_name: current_duration, BROADCAST_START_TIME_KEY:broadcast_start_time,IS_COMPLETED_KEY: false});
 
   }
   //ALREADY BROADCAST ADDED BUT THE CONTENT KEY IS FOR FIRST TIME
@@ -389,16 +164,16 @@ Future addtoRecordOnNormalCondition(
     double current_duration =
         (buffer_box.get(broadcast_id))[zone][CONTENT_DURATION_KEY];
     recorded_data[content_name] = current_duration;
-    recorded_box.put(broadcast_id, recorded_data);
+   await recorded_box.put(broadcast_id, recorded_data);
   }
   //ALREADY BROADCAST ADDED ALSO CONTENT ADDED
   else if (recorded_data.containsKey(content_name)) {
-    print("/ALREADY BROADCAST ADDED ALSO CONTENT ADDED");
-    double past_duration = recorded_data[content_name];
+    print("ALREADY BROADCAST ADDED ALSO CONTENT ADDED");
+    double past_duration = (recorded_data[content_name]).toDouble();
     double current_duration = (buffer_box.get(broadcast_id))[zone][CONTENT_DURATION_KEY];
     double updated_duration = past_duration + current_duration;
     recorded_data[content_name] = updated_duration;
-    recorded_box.put(broadcast_id, recorded_data);
+   await recorded_box.put(broadcast_id, recorded_data);
   }
 }
 
@@ -414,16 +189,20 @@ Future addtoRecordFromBufferOnEnd() async {
     Map? bufferdata = buffer_box.get(broadcast_id);
     DateTime dateTimeNow = DateTime.now();
        Map? recorded_data = recorded_box.get(broadcast_id);
-       print("dataaaaaaaaaa = $recorded_data");
+       print("recorded_data = $recorded_data");
     if (bufferdata != null) {
       Map? recorded_data = recorded_box.get(broadcast_id);
-      print('recorded_data = $recorded_data');
-
+     print("buffer data"+bufferdata.toString());
       bufferdata.forEach((contentName, value) {
-          print(" bufferdata.forEach key = $contentName");
+
+        if(contentName != "broadcast_start_datetime")
+        {
+
+                   print(" bufferdata.forEach key = $contentName");
           print(" bufferdata.forEach value = $value");
+          
           String content_name = value[CONTENT_NAME_KEY];
-          DateTime start_time = value[START_TIME_KEY];
+          var start_time = value[START_TIME_KEY];
 
           int current_duration = dateTimeNow.difference(start_time).inSeconds;
           print("recordede box data = $recorded_data");
@@ -451,11 +230,17 @@ Future addtoRecordFromBufferOnEnd() async {
 
             recorded_box.put(broadcast_id, recorded_data);
           }
+
+        }
+ 
         },
       );
 
       Map<dynamic,dynamic> recorded_data_after_complete = {...recorded_box.get(broadcast_id)};
       recorded_data_after_complete[IS_COMPLETED_KEY] = true;
+      DateTime end_time= await last_saved_time_box.get(LAST_SAVED_TIME_KEY);
+      recorded_data_after_complete[BROADCAST_END_TIME_KEY]=end_time;
+
       recorded_box.put(broadcast_id, recorded_data_after_complete);
       buffer_box.delete(broadcast_id);
 
@@ -478,7 +263,7 @@ void addToServer() async {
       Map recorded_data = recorded_box.get(element);
       print("recoreded data  = $recorded_data");
 
-      print(recorded_data);
+
       if (recorded_data[IS_COMPLETED_KEY]) {
         final prefs = await SharedPreferences.getInstance();
         String? screenCode = prefs.getString("NewScreenCode");
@@ -487,13 +272,13 @@ void addToServer() async {
 
         jsonData["broadcast_id"] = element;
 
-        jsonData["broadcast_start_datetime"] = "2023-12-03T10:00:00Z";
-        jsonData["broadcast_end_datetime"] = "2023-12-03T10:00:00Z";
+        jsonData["broadcast_start_datetime"] = recorded_data[BROADCAST_START_TIME_KEY].toString();
+        jsonData["broadcast_end_datetime"] = recorded_data[BROADCAST_END_TIME_KEY].toString();
 
         List content_history = [];
 
         recorded_data.forEach((key, value) {
-          if (key != IS_COMPLETED_KEY) {
+          if (key != IS_COMPLETED_KEY && key != BROADCAST_END_TIME_KEY && key != BROADCAST_START_TIME_KEY) {
             Map content_data = {};
 
             content_data["content_name"] = key;
@@ -508,6 +293,9 @@ void addToServer() async {
         // API CALLL;
 
         // API CALLING
+        print("json before decodingggggg");
+
+        print(jsonData);
         print("send dataaaaaaaaaaaaaaaaaaaaaaaaaaaa = ${jsonEncode(jsonData)}");
 
          ContentLogsRepository().sendContentLogs(jsonData);
@@ -538,6 +326,9 @@ void printValues() {
 
 //   broadcast_id:    {
 
+// BROADCAST_START_TIME_KEY:"00:00:00",
+// BROADCAST_END_TIME_KEY:"00:00:00"
+
 //     zoneno : {
 //               CONTENT_NAME_KEY:contentname,
 //               CONTENT_DURATION_KEY:contentDuration,
@@ -560,7 +351,10 @@ void printValues() {
 
 // recorded_data={
 //   broadcast_id : {
-//     IS_COMPLETED_KEY:true
+// BROADCAST_START_TIME_KEY:"00:00:00",
+// BROADCAST_END_TIME_KEY:"00:00:00"
+// IS_COMPLETED_KEY:true
+
 
 //     CONTENT_NAME_KEY: contentDuration
 
